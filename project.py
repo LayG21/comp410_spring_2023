045c400f6d181b92e68668b4da62e32622bd8dde
Jalen Shine <89696110+MrUnkn0wnGuy@users.noreply.github.com>
2023-02-04 00:48:28 -0500

    Add files via upload
    Input text (states) now produces output text in the desired abbreviated and sorted frequency list.

--- project.py
+++ project.py
@@ -1,86 +1,3 @@
-"""COMP-410 Spring 2023 Class Project"""
-import requests
-
-def add_two_numbers(num1, num2):
-    """Adds two numbers together"""
-    return num1 + num2
-
-def convert_text_numbers_to_integers(text_numbers:str) -> list:
-    """Convert comma separated list of numbhers zero, one, two, three, four, five, six, 
-    seven, eight, nine to integers"""
-    text_numbers = text_numbers.split(',')
-    numbers = []
-    for number in text_numbers:
-        number = number.strip()
-        if number == 'zero':
-            numbers.append(0)
-        elif number == 'one':
-            numbers.append(1)
-        elif number == 'two':
-            numbers.append(2)
-        elif number == 'three':
-            numbers.append(3)
-        elif number == 'four':
-            numbers.append(4)
-        elif number == 'five':
-            numbers.append(5)
-        elif number == 'six':
-            numbers.append(6)
-        elif number == 'seven':
-            numbers.append(7)
-        elif number == 'eight':
-            numbers.append(8)
-        elif number == 'nine':
-            numbers.append(9)
-        else:
-            raise ValueError(f'Unknown number {number}')
-    return numbers
-
-
-def convert_text_to_digits_example(text: str) -> list:
-    """Converts a comma seperated list of text digits to integers"""
-    text_digits = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-                   'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-
-    results = []
-    for digit in text.split(','):
-        digit = digit.strip()
-        if digit in text_digits:
-            results.append(text_digits[digit])
-        else:
-            raise ValueError('Invalid digit: ' + digit)
-
-    return results
-
-
-def show_aggie_pride():
-    """Show Aggie Pride"""
-    return 'Aggie Pride - Worldwide'
-
-
-def reverse_list(input_list) -> list:
-    """Returns a list in reverse order"""
-    return input_list[::-1]
-
-def get_area_codes() -> dict:
-    """Returns a dict of known area codes"""
-    # Get the area code information from NANPA
-    url = requests.get('https://nationalnanpa.com/nanp1/npa_report.csv', timeout=10)
-    area_codes = {}
-    for line in url.text.split('\n'):
-        # skip the header lines
-        if line.startswith('NPA') or line.startswith('File Date') or not line:
-            continue
-        ac_info = line.split(',')
-        area_codes[ac_info[0]] = ac_info[8]
-    return area_codes
-
-
-if __name__ == '__main__':
-    print(show_aggie_pride())
-
-
-
 """
 Create a function which inputs a comma separated list of states and returns a dict of states and 
     total number of times each state was seen.
@@ -99,13 +16,11 @@ Expected results
 {'AK': 1, 'NC':2, 'NJ':1, 'NY':1, 'WA':1}
 
 Step 1: Prepping the input (Kamaria P.)
-Input is a string named as input_states as function parameter (DONE)
+Input is a string
 Input needs to be broken up/separated to commas (also worry about spacing; some sort of trimming)
-    - Input needs to be turned into a list named states
-Need a dict of all written out states (DONE)
-A dictionary for each state name and mapping its abbreviation to it State_to_abb_dict (DONE)
+Need a dict of all written out states
+A dictionary for each state name and mapping its abbreviation to it State_to_abb_dict
 A dictionary for state frequencies (how many times it appears in input) state_freq_dict
-
 
 Step 2: Iterate through input and check for valid state names (Jalen L.)
 For loop to iterate through each state in "text" variable for state in states
@@ -119,7 +34,22 @@ For loop to iterate through each state in "text" variable for state in states
         -if not:
             - raise ValueError exception
 return sorted(state_freq_dict)
+
+Import 
+
+Julious H.
+Test cases:
+- Misspelled state(s) -> Value Error
+- Empty list -> ValueError?
+
+Jalen S. (DONE)
+- All correct states will produced a abbreviation frequency list (DONE)
+    - Input text (DONE)
+        text = 'Alaska,North Carolina,  New York , New Jersey, North Carolina, Washington' (DONE)
+        Expected results {'AK': 1, 'NC':2, 'NJ':1, 'NY':1, 'WA':1} (DONE)
+
 """
+
 #Dictionary of States to Abbrev.
 state_to_abb_dict = {
   "Alabama": "AL",
@@ -182,11 +112,20 @@ state_to_abb_dict = {
 
 }
 
-#User input and adds commas adding input to state_freq_dict 
-states = input("enter state names: ").split()
+#Get user input (Must have commas separating state names)
+states = input("Enter state names: ")
+
+#Iterate through input and replace names with abbreviations
+for key in state_to_abb_dict.keys():
+    states = states.replace(key, state_to_abb_dict[key])
+#Split input by commas and stripped whitespace
+states = [x.strip() for x in states.split(",")]
+#Sorted the abbreviated states
+sorted_states = sorted(states)
+
 state_freq_dict = {}
 #iterated over list
-for item in states:
+for item in sorted_states:
     #checking if item is in dictionary
     if item in state_freq_dict:
         #incrementing if repeated
@@ -195,23 +134,9 @@ for item in states:
         #set to 1 if not repeated
         state_freq_dict[item] = 1
 
+#Print sorted abbreviated list
+print(state_freq_dict)
 
-def get_state_abbrev_freq(text_states: str) -> dict:
-    #if input was empty <--- not too sure if this will work but test is needed
-    if input == "" or states == []:
-        raise ValueError('Empty List')
-
-    #non empty input
-    for state in states:
-        #check for valid state names
-        if state in state_to_abb_dict:
-            #check if state has already been added to abbreviation frequency dict
-            if state_to_abb_dict[state] in state_freq_dict:
-                state_freq_dict[state_to_abb_dict[state]] += 1
-            #if not, add it
-            else:
-                state_freq_dict[state_to_abb_dict[state]] = 1
-        #If invalid state name, raise ValueError
-        else:
-            raise ValueError('Invalid State: ' + state) 
-    return sorted(state_freq_dict)
+"""Had to comment this out because it was causing an indentation error"""
+#if input was empty <--- not too sure if this will work but test is needed
+#def get_state_abbrev_freq(text_states: str) -> dict:
