be451693c8c8347577ee5416138dd0cff0171215
LayG21 <91238858+LayG21@users.noreply.github.com>
2023-02-14 02:11:10 +0000

    More test cases added and bool statement fixed

--- project.py
+++ project.py
@@ -106,7 +106,8 @@ def area_code_lookup(phone_nums:str) -> dict:
     output_dict = {}
     for num in phone_list:
         area_code = num[0:3]
-        if ~bool(re.match(num, '/\d{3}-\d{3}-\d{4})/')) & (int(area_code) > 200):
+        #fixed regex and match statement
+        if bool(re.match(r"(\d{3}-\d{3}-\d{4})",num)) & (int(area_code) > 200):
             output_dict[int(area_code)] = ac_dict[area_code]
         else:
             raise ValueError('Invalid phone number: ' + num)
